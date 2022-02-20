3725bf380879406971c62cd6a4a102b0f9475367
Thomas Gale <thomasgale2005@gmail.com>
2022-02-20 23:46:48 +0000

    commit on master

--- RobotTest/RobotTest.ino
+++ RobotTest/RobotTest.ino
@@ -2,7 +2,7 @@
   by BARRAGAN <http://barraganstudio.com>
   This example code is in the public domain.
 
-  modified 8 Nov 2013
+  modified 8 Nov 20b b13
   by Scott Fitzgerald
   https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
 */
@@ -13,12 +13,122 @@ Servo myservo10;
 Servo myservo11;
 Servo myservo12;
 Servo myservo13;
-float mg(float ar[3]){
+extern Servo myservo9;
+extern Servo myservo10;
+extern Servo myservo11;
+extern Servo myservo12;
+extern Servo myservo13;
+float mg(float ar[3]) {
   float mag = 0;
   for (int i = 0; i < 3; i++) {
-    mag = mag + ar[i]*ar[i];
+    mag = mag + ar[i] * ar[i];
   }
-  return(sqrt(mag));
+  return (sqrt(mag));
+}
+//(a[0]*b[0]+a[1]*b[1]+a[2]*b[2])/(sqrt(a[0]*a[0]+a[1]*a[1]+a[2]*a[2])*sqrt(b[0]*b[0]+b[1]*b[1]+b[2]*b[2]))
+float qd(float a, float b, float c, int d) {
+  float out = {(-b + d * sqrt(b * b - 4 * a * c)) / (2 * a)};
+  return (out);
+}
+void moveBot(float L[5], float TP[3], float TRA[2]) {
+  float O[3] = {0, 0, L[4]};
+  float TRV[3] = {cos(TRA[0]*PI / 180)*cos(TRA[1]*PI / 180), sin(TRA[0]*PI / 180)*cos(TRA[1]*PI / 180), sin(TRA[1]*PI / 180)};
+  float P1[3] = {TRV[0]*L[0] / mg(TRV) + TP[0], TRV[1]*L[0] / mg(TRV) + TP[1], TRV[2]*L[0] / mg(TRV) + TP[2]};
+  float OP1[3] = {P1[0], P1[1], P1[2] - O[2]};
+  float N[3] = { -TRV[2]*P1[0] / (P1[0]*TRV[0] + P1[1]*TRV[1]), -TRV[2] / (P1[0]*TRV[0] / P1[1] + TRV[1]), 1};
+  float Q[3] = { -N[2]*OP1[0] / (OP1[0]*N[0] + OP1[1]*N[1]), -N[2] / (OP1[0]*N[0] / OP1[1] + N[1]), 1};
+  float P2[3] = {Q[0]*L[1] / mg(Q) + P1[0], Q[1]*L[1] / mg(Q) + P1[1], Q[2]*L[1] / mg(Q) + P1[2]};
+  float P3[3] = {0, 0, 0};
+  float P4[3] = {0, 0, L[4]};
+  int options[8] = {1, 1, 1, -1, -1, -1, -1, 1};
+  for (int i = 0; i < 4; i++) {
+    float P3Z = qd((2 * P2[2] - 2 * L[4]) * (2 * P2[2] - 2 * L[4]) + 4 * (P2[0] * P2[0] + P2[1] * P2[1]),
+                   4 * (L[4] - P2[2]) * (-L[2] * L[2] + L[3] * L[3] - L[4] * L[4] + mg(P2) * mg(P2)) - 8 * L[4] * (P2[0] * P2[0] + P2[1] * P2[1]),
+                   (-L[2] * L[2] + L[3] * L[3] - L[4] * L[4] + mg(P2) * mg(P2)) * (-L[2] * L[2] + L[3] * L[3] - L[4] * L[4] + mg(P2) * mg(P2)) - 4 * (P2[0] * P2[0] + P2[1] * P2[1]) * (L[3] * L[3] - L[4] * L[4]), options[2 * i]);
+    float P3Y = options[2 * i + 1] * sqrt((L[3] * L[3] - (P3Z - L[4]) * (P3Z - L[4])) / (((P2[0] * P2[0]) / (P2[1] * P2[1])) + 1));
+    float P3X = P3Y * P2[0] / P2[1];
+    float tempP3P2[3] = {P3X - P2[0], P3Y - P2[1], P3Z - P2[2]};
+    if ((round(mg(tempP3P2) * 10000) == L[2] * 10000) and (round(mg(tempP3P2) * 10000) == L[3] * 10000)) {
+      if (P3X >= 0) {
+        if (P2[2] <= (P2[0]*P3Z / P3X + P4[2])) {
+          P3[0] = P3X;
+          P3[1] = P3Y;
+          P3[2] = P3Z;
+        }
+      } else {
+        if (P2[2] >= (P2[0]*P3Z / P3X + P4[2])) {
+          P3[0] = P3X;
+          P3[1] = P3Y;
+          P3[2] = P3Z;
+        }
+      }
+    }
+  }
+  float P1TP[3] = {TP[0] - P1[0], TP[1] - P1[1], TP[2] - P1[2]};
+  float TPP1[3] = {P1[0] - TP[0], P1[1] - TP[1], P1[2] - TP[2]};
+  float P1P2[3] = {P2[0] - P1[0], P2[1] - P1[1], P2[2] - P1[2]};
+  float P2P1[3] = {P1[0] - P2[0], P1[1] - P2[1], P1[2] - P2[2]};
+  float P2P3[3] = {P3[0] - P2[0], P3[1] - P2[1], P3[2] - P2[2]};
+  float P3P2[3] = {P2[0] - P3[0], P2[1] - P3[1], P2[2] - P3[2]};
+  float P3P4[3] = {P4[0] - P3[0], P4[1] - P3[1], P4[2] - P3[2]};
+  float P4P3[3] = {P3[0] - P4[0], P3[1] - P4[1], P3[2] - P4[2]};
+  float angles[5] = {
+    acos((P1TP[0]*P1P2[0] + P1TP[1]*P1P2[1] + P1TP[2]*P1P2[2]) / (sqrt(P1TP[0]*P1TP[0] + P1TP[1]*P1TP[1] + P1TP[2]*P1TP[2])*sqrt(P1P2[0]*P1P2[0] + P1P2[1]*P1P2[1] + P1P2[2]*P1P2[2]))) * 180 / PI,
+    acos((P2P3[0]*P2P1[0] + P2P3[1]*P2P1[1] + P2P3[2]*P2P1[2]) / (sqrt(P2P3[0]*P2P3[0] + P2P3[1]*P2P3[1] + P2P3[2]*P2P3[2])*sqrt(P2P1[0]*P2P1[0] + P2P1[1]*P2P1[1] + P2P1[2]*P2P1[2]))) * 180 / PI,
+    acos((P3P4[0]*P3P2[0] + P3P4[1]*P3P2[1] + P3P4[2]*P3P2[2]) / (sqrt(P3P4[0]*P3P4[0] + P3P4[1]*P3P4[1] + P3P4[2]*P3P4[2])*sqrt(P3P2[0]*P3P2[0] + P3P2[1]*P3P2[1] + P3P2[2]*P3P2[2]))) * 180 / PI,
+    acos((P4P3[2] * -1) / sqrt(P4P3[0]*P4P3[0] + P4P3[1]*P4P3[1] + P4P3[2]*P4P3[2])) * 180 / PI,
+    acos(-P3[0]/sqrt(P3[0]*P3[0]+P3[1]*P3[1])) * 180 / PI
+  };
+  if (P1[0] >= 0) {
+    if (TPP1[0]*P1[1] / P1[0] - TPP1[1] >= 0) {
+      angles[0] = 360 - angles[0];
+    }
+  } else {
+    if (TPP1[0]*P1[1] / P1[0] - TPP1[1] <= 0) {
+      angles[0] += 180;
+    }
+  }
+  if (P3[0] <= 0) {
+    angles[3] = 360 - angles[3];
+    if (P3[1] <= 0) {
+      angles[4] = acos(P3[0]/sqrt(P3[0]*P3[0]+P3[1]*P3[1])) * 180 / PI;
+    } else {
+      angles[4] = 360 - acos(P3[0]/sqrt(P3[0]*P3[0]+P3[1]*P3[1])) * 180 / PI;
+    }
+  } else {
+    if (P3[1] <= 0) {
+      angles[4] = 360 - angles[4];
+    }
+  }
+  if (P2[0] - P3[0] >= 0) {
+    if (P1[2] >= P2[2] + (P1[0] - P2[0]) * (P2[2] - P3[2]) / (P2[0] - P3[0])) {
+      angles[1] = 360 - angles[1];
+    }
+  } else {
+    if (P1[2] <= P2[2] + (P1[0] - P2[0]) * (P2[2] - P3[2]) / (P2[0] - P3[0])) {
+      angles[1] = 360 - angles[1];
+    }
+  }
+  float servo10 = (360-angles[0]); //A1
+  float servo9 = (angles[1]); //A2
+  float servo11 = (angles[2]); //A3
+  float servo12 = (360-angles[3]); //A4
+  float servo13 = (360-angles[4]); //A5
+  myservo9.write(2*servo9/3-27);
+  myservo10.write(2*servo10/3-27);
+  myservo11.write(2*servo11/3-27);
+  myservo12.write(2*servo12/3-27);
+  myservo13.write(2*servo13/3-27);
+}
+void moveBotLinePath(float a[3],float b[3],float angs[2],float L[5],int split) {
+  float ab[3] = {b[0]-a[0],b[1]-a[1],b[2]-a[2]};
+  float TP[3] = {0,0,0};
+  for (int i = 0; i < split; i++) {
+    TP[0] = a[0]+ab[0]*i/split;
+    TP[1] = a[1]+ab[1]*i/split;
+    TP[2] = a[2]+ab[2]*i/split;
+    moveBot(L,TP,angs);
+  }
 }
 float servoSmoothed9;
 float switchPrevious9;
@@ -35,42 +145,36 @@ float servo12;
 float servoSmoothed13;
 float switchPrevious13;
 float servo13;
-//Varing:
-float robtDm[5] = {5.5,6.25,22.25,22.25,6.25};
-float TP[3] = {0,30,30};
-float TRA[2] = {90,0};
-//Math Start
-float O[3] = {0,0,robtDm[4]};
-float TRV[3] = {cos(TRA[0])*cos(TRA[1]),sin(TRA[0])*cos(TRA[1]),sin(TRA[1])};
-float P1[3] = {TRV[0]*robtDm[0]/mg(TRV)+TP[0],TRV[1]*robtDm[0]/mg(TRV)+TP[1],TRV[2]*robtDm[0]/mg(TRV)+TP[2]};
-float OP1[3] = {P1[0]-O[0],P1[1]-O[1],P1[2]-O[2]};
-float N[3] = {-TRV[2]*OP1[0]/(OP1[0]*TRV[0]+OP1[1]*TRV[1]),-TRV[2]/(OP1[0]*TRV[0]/OP1[1]+TRV[1]),1};
-float Q[3] = {-N[2]*OP1[0]/(OP1[0]*N[0]+OP1[1]*N[1]),-N[2]/(OP1[0]*N[0]/OP1[1]+N[1]),1};
-float P2[3] = {Q[0]*robtDm[1]/mg(Q)+P1[0],Q[1]*robtDm[1]/mg(Q)+P1[1],Q[2]*robtDm[1]/mg(Q)+P1[2]};
-//Math End
-int VRx1 = A0;
-int VRy1 = A1;
-int VRx2 = A2;
-int VRy2 = A3;
-int VRx3 = A4;
-int VRy3 = A5;s
 
-int xPosition1 = 0;
-int yPosition1 = 0;
-int xPosition2 = 0;
-int yPosition2 = 0;
-int xPosition3 = 0;
-int yPosition3 = 0;
-int mapX1 = 0;
-int mapY1 = 0;
-int mapX2 = 0;
-int mapY2 = 0;
-int mapX3 = 0;
-int mapY3 = 0;
+//Varing:
+float L[5] = {6, 7.5, 22.25, 22.25, 3.75};
+float TP[3] = {15.000001, 5.000001, 25.0000001};
+float TRA[2] = {0.5, 135.5};
+
+float VRx1 = A0;
+float VRy1 = A1;
+float VRx2 = A2;
+float VRy2 = A3;
+float VRx3 = A4;
+float VRy3 = A5;
+
+float xPosition1 = 0;
+float yPosition1 = 0;
+float xPosition2 = 0;
+float yPosition2 = 0;
+float xPosition3 = 0;
+float yPosition3 = 0;
+float mapX1 = 0;
+float mapY1 = 0;
+float mapX2 = 0;
+float mapY2 = 0;
+float mapX3 = 0;
+float mapY3 = 0;
+float a[3] = {0,0,0};
+float b[3] = {0,0,0};
 // twelve servo objects can be created on most boards
 
 int pos = 0;    // variable to store the servo position
-
 void setup() {
   Serial.begin(9600);
   pinMode(VRx1, INPUT);
@@ -93,41 +197,123 @@ void loop() {
   yPosition2 = analogRead(VRy2);
   xPosition3 = analogRead(VRx3);
   yPosition3 = analogRead(VRy3);
-  mapX1 = map(xPosition1, 0, 1023, -180, 180);
-  mapY1 = map(yPosition1, 0, 1023, -180, 180);
-  mapX2 = map(xPosition2, 0, 1023, -180, 180);
-  mapY2 = map(yPosition2, 0, 1023, -180, 180);
-  mapX3 = map(xPosition3, 0, 1023, -180, 180);
-  mapY3 = map(yPosition3, 0, 1023, -180, 180);
-  servo9 = (-mapX2 / 2.75 + 95);
-  servo10 = (mapY2 / 2.75 + 90);
-  servo11 = (mapY1 / 4 + 95);
-  servo12 = (-mapY3 / 4 + 100);
-  servo13 = (mapX3 / 4 + 90);
-//  servoSmoothed9 = (servo9*0.02)+(switchPrevious9*0.98);
-//  switchPrevious9 = servoSmoothed9;
-//  servoSmoothed10 = (servo10*0.02)+(switchPrevious10*0.98);
-//  switchPrevious10 = servoSmoothed10;
-//  servoSmoothed11 = (servo11*0.05)+(switchPrevious11*0.99);
-//  switchPrevious11 = servoSmoothed11;
-//  servoSmoothed12 = (servo12*0.01)+(switchPrevious12*0.99);
-//  switchPrevious12 = servoSmoothed12;
-//  servoSmoothed13 = (servo13*0.01)+(switchPrevious13*0.99);
-//  switchPrevious13 = servoSmoothed13;
-//  myservo9.write(servoSmoothed9);
-//  myservo10.write(servoSmoothed10);
-//  myservo11.write(servoSmoothed11);
-//  myservo12.write(servoSmoothed12);
-//  myservo13.write(servoSmoothed13);
-  myservo9.write(servo9);
-  myservo10.write(servo10);
-  myservo11.write(servo11);
-  myservo12.write(servo12);
-  myservo13.write(servo13);
-//  delay(5);
-//  myservo13.write(90);
-  //  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-  //    // in steps of 1 degree 
+  mapX1 = map(xPosition1, 0, 1023, -511.5, 511.5);
+  mapY1 = map(yPosition1, 0, 1023, -511.5, 511.5);
+  mapX2 = map(xPosition2, 0, 1023, -511.5, 511.5);
+  mapY2 = map(yPosition2, 0, 1023, -511.5, 511.5);
+  mapX3 = map(xPosition3, 0, 1023, -511.5, 511.5);
+  mapY3 = map(yPosition3, 0, 1023, -511.5, 511.5);
+  float TP[3] = {25, 0, 17.5};
+  float TRA[2] = {359.9, 179.9};
+  moveBot(L, TP, TRA);
+//  TP[0] += mapX1/10000;
+//  TP[1] += mapY1/10000;
+//  TP[2] += mapX2/10000;
+//  TRA[0] += mapY3/10000;
+//  TRA[1] += mapX3/10000;
+//  if (TRA[0] > 360) {
+//    TRA[0] = 1.5;
+//  } else if (TRA[0]<0) {
+//    TRA[0] = 358.5;
+//  }
+//  if (TRA[1] > 360) {
+//    TRA[1] = 1.5;
+//  } else if (TRA[1]<0) {
+//    TRA[1] = 358.5;
+//  }
+  delay(1500);
+  a[0] = 25;
+  a[1] = 0;
+  a[2] = 17.5;
+  b[0] = 25;
+  b[1] = 0;
+  b[2] = 25;
+  moveBotLinePath(a,b,TRA,L,5);
+  delay(1500);
+  a[0] = 25;
+  a[1] = 0;
+  a[2] = 25;
+  b[0] = 25;
+  b[1] = 7.5;
+  b[2] = 25;
+  moveBotLinePath(a,b,TRA,L,5);
+  delay(1500);
+  a[0] = 25;
+  a[1] = 0;
+  a[2] = 25;
+  b[0] = 25;
+  b[1] = 7.5;
+  b[2] = -5;
+  moveBotLinePath(a,b,TRA,L,15);
+  delay(2000);
+  a[0] = 25;
+  a[1] = 7.5;
+  a[2] = -5;
+  b[0] = 25;
+  b[1] = 15;
+  b[2] = -5;
+  moveBotLinePath(a,b,TRA,L,5);
+  delay(1500);
+  a[0] = 25;
+  a[1] = 15;
+  a[2] = -5;
+  b[0] = 25;
+  b[1] = 15;
+  b[2] = -15;
+  moveBotLinePath(a,b,TRA,L,5);
+  delay(1500);
+  a[0] = 25;
+  a[1] = 15;
+  a[2] = -15;
+  b[0] = 25;
+  b[1] = -15;
+  b[2] = -15;
+  moveBotLinePath(a,b,TRA,L,15);
+  delay(2000);
+  a[0] = 25;
+  a[1] = -15;
+  a[2] = -15;
+  b[0] = 25;
+  b[1] = -15;
+  b[2] = -5;
+  moveBotLinePath(a,b,TRA,L,5);
+  delay(1500);
+  a[0] = 25;
+  a[1] = -15;
+  a[2] = -5;
+  b[0] = 25;
+  b[1] = -7.5;
+  b[2] = -5;
+  moveBotLinePath(a,b,TRA,L,5);
+  delay(1500);
+  a[0] = 25;
+  a[1] = -7.5;
+  a[2] = -5;
+  b[0] = 25;
+  b[1] = -7.5;
+  b[2] = 25;
+  moveBotLinePath(a,b,TRA,L,15);
+  delay(2000);
+  a[0] = 25;
+  a[1] = -7.5;
+  a[2] = 25;
+  b[0] = 25;
+  b[1] = 0;
+  b[2] = 25;
+  moveBotLinePath(a,b,TRA,L,5);
+  delay(1500);
+  a[0] = 25;
+  a[1] = 0;
+  a[2] = 25;
+  b[0] = 25;
+  b[1] = 0;
+  b[2] = 17.5;
+  moveBotLinePath(a,b,TRA,L,5);
+  delay(1500);
+//  moveBot(L, {15, 5, 25}, {340.5, 135.5});
+  //  myservo13.write(90);
+  //  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees  
+  //    // in steps of 1 degree
   //    myservo.write(pos);              // tell servo to go to position in variable 'pos'
   //    delay(15);                       // waits 15 ms for the servo to reach the position
   //  }
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from tkinter import E
 from turtle import bgcolor
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -8,7 +9,7 @@ def sin(x):
 def cos(x):
     return(round(math.cos(math.radians(x)),5))
 def acos(x):
-    return(round(math.degrees(math.acos(x),5)))
+    return(round(math.degrees(math.acos(x)),5))
 def atan(x):
     return(round(math.degrees(math.atan(x),5)))
 def mg(vec):
@@ -17,7 +18,6 @@ def mg(vec):
         mag+=vec[i]**2
     return(mag**0.5)
 def qd(a,b,c):
-    print([a,b,c])
     E1 = (-b + (b**2-4*a*c)**0.5)/(2*a)
     E2 = (-b - (b**2-4*a*c)**0.5)/(2*a)
     return([E1,E2])
@@ -27,21 +27,32 @@ def vecSum(a,b):
     return(a[0]+b[0],a[1]+b[1],a[2]+b[2])
 def vecSub(a,b):
     return(a[0]-b[0],a[1]-b[1],a[2]-b[2])
+def vecScale(a,b):
+    return([a[0]*b,a[1]*b,a[2]*b])
+def calcP3(L,P2,P3Ops):
+    P3Z = qd((2*P2[2]-2*L[4])**2+4*(P2[0]**2+P2[1]**2),
+    4*(L[4]-P2[2])*(-L[2]**2+L[3]**2-L[4]**2+mg(P2)**2)-8*L[4]*(P2[0]**2+P2[1]**2),
+    (-L[2]**2+L[3]**2-L[4]**2+mg(P2)**2)**2-4*(P2[0]**2+P2[1]**2)*(L[3]**2-L[4]**2))[P3Ops[0]]
+    P3Y = [((L[3]**2-(P3Z-L[4])**2)/((P2[0]**2/P2[1]**2)+1))**0.5,-((L[3]**2-(P3Z-L[4])**2)/((P2[0]**2/P2[1]**2)+1))**0.5][P3Ops[1]]
+    P3X = P3Y*P2[0]/P2[1]
+    return([P3X,P3Y,P3Z])
+a = 25
 fig = plt.figure()
 ax = Axes3D(fig)
-ax.set_xlim3d(0, 150)
-ax.set_ylim3d(0, 150)
-ax.set_zlim3d(0, 150)
+ax.set_xlim3d(0, a+10)
+ax.set_ylim3d(0, a+10)
+ax.set_zlim3d(0, a+10)
 ax.set_box_aspect([ub - lb for lb, ub in (getattr(ax, f'get_{a}lim')() for a in 'xyz')])
 
 # Varing:
-# L = [5.5,6.25,22.25,22.25,6.25]
-L = [10.5,10.25,22.25,22.25,6.25]
-TP = [10,10,20]
-TRA = [20,70]
+L = [5.5,6.25,22.25,22.25,6]
+# L = [10.5,10.25,22.25,22.25,6]
+TP = [15,5,25]
+TRA = [0.5,135.5] # [longitude,latitude]
 # Vectors:
 O = [0,0,L[4]]
 TRV = [cos(TRA[0])*cos(TRA[1]),sin(TRA[0])*cos(TRA[1]),sin(TRA[1])]
+print(TRV[0])
 P1 = [TRV[0]*L[0]/mg(TRV)+TP[0],TRV[1]*L[0]/mg(TRV)+TP[1],TRV[2]*L[0]/mg(TRV)+TP[2]]
 OP1 = [P1[0]-O[0],
     P1[1]-O[1],
@@ -55,26 +66,65 @@ Q = [-N[2]*OP1[0]/(OP1[0]*N[0]+OP1[1]*N[1]),
 P2 = [Q[0]*L[1]/mg(Q)+P1[0],
     Q[1]*L[1]/mg(Q)+P1[1],
     Q[2]*L[1]/mg(Q)+P1[2]]
-P3Ops = [0,0]
-P3Z = qd((2*P2[2]-2*L[4])**2+4*(P2[0]**2+P2[1]**2),
-    4*(L[4]-P2[2])*(-L[2]**2+L[3]**2-L[4]**2+mg(P2)**2)-8*L[4]*(P2[0]**2+P2[1]**2),
-    (-L[2]**2+L[3]**2-L[4]**2+mg(P2)**2)**2-4*(P2[0]**2+P2[1]**2)*(L[3]**2-L[4]**2))[P3Ops[0]]
-P3Y = [((L[3]**2-(P3Z-L[4])**2)/((P2[0]**2/P2[1]**2)+1))**0.5,-((L[3]**2-(P3Z-L[4])**2)/((P2[0]**2/P2[1]**2)+1))**0.5][P3Ops[1]]
-P3 = [P3Y*P2[0]/P2[1],
-    P3Y,
-    P3Z]
+P3Ops = [1,1] # <-- 0 for positive 1 for negative on quadratic
 P4 = [0,0,L[4]]
-print([mg(vecSub(P2,P3)),mg(vecSub(P3,P4))], ([L[2],L[3]]))
-print(P3)
+P3s = [calcP3(L,P2,[0,0]),calcP3(L,P2,[0,1]),calcP3(L,P2,[1,0]),calcP3(L,P2,[1,1])]
+P3t = []
+Lprec= 4
+i = 0
+while i < 4:
+    if round(mg(vecSub(P2,P3s[i])),Lprec) == L[2] and round(mg(vecSub(P4,P3s[i])),Lprec) == L[3]:
+        if P3s[i][0] > 0: #P3 Forward
+            if P2[2] < (P2[0]*P3s[i][2]/P3s[i][0]+P4[2]):
+                P3 = P3s[i]
+        else: #P3 Back
+            if P2[2] > (P2[0]*P3s[i][2]/P3s[i][0]+P4[2]):
+                P3 = P3s[i]
+    i += 1
 # Angles:
-angles = []
+angles = [acos((dt(vecScale(TRV,-1),vecSub(P2,P1)))/(mg(vecScale(TRV,-1))*mg(vecSub(P2,P1)))), #A1
+    acos((dt(vecSub(P3,P2),vecSub(P1,P2)))/(mg(vecSub(P3,P2))*mg(vecSub(P1,P2)))), #A2
+    acos((dt(vecSub(P4,P3),vecSub(P2,P3)))/(mg(vecSub(P4,P3))*mg(vecSub(P2,P3)))), #A3
+    acos((dt(vecSub(P3,P4),[0,0,-1]))/(mg(vecSub(P3,P4)))), #A4
+    acos(-P3[0]/mg([P3[0],P3[1],0])) #A5
+]
+print("origin " + str(angles[4]))
+#need to change angles to add 180 or not(A1,A2,A4) (+-90 on A5)
+if dt([P1[0],P1[1],0],[1,0,0])>0: #P1 forwards
+    if dt(vecScale(TRV,-1),[-P1[1]/P1[0],1,0])>0:
+        #End clockwise
+        angles[0] = 360 - angles[0]
+else: #P1
+    if dt(vecScale(TRV,-1),[-P1[1]/P1[0],1,0])<0:
+        #End clockwise
+        angles[0] += 180
+if P3[0] < 0 :
+    angles[3] = 360-angles[3]
+    if P3[1] < 0:
+        angles[4] = acos(P3[0]/mg([P3[0],P3[1],0]))
+    else:
+        angles[4] = 360 - acos(P3[0]/mg([P3[0],P3[1],0]))
+    print("Trigger " + str(angles[4]))
+else:
+    if P3[1] < 0:
+        angles[4] = 360 - angles[4]
+if P2[0]-P3[0] > 0:
+    if P1[2] > P2[2]+(P1[0]-P2[0])*(P2[2]-P3[2])/(P2[0]-P3[0]):
+        angles[1] = 360-angles[1]
+else:
+    if P1[2] < P2[2]+(P1[0]-P2[0])*(P2[2]-P3[2])/(P2[0]-P3[0]):
+        angles[1] = 360-angles[1]
+print(P3)
+print(angles)
+print(mg(vecSub(P2,P3)))
+print(mg(vecSub(P4,P3)))
 # Plotting:
 plt.plot([TP[0]+2,TP[0],TP[0],TP[0],TP[0]],[TP[1],TP[1],TP[1]+2,TP[1],TP[1]],[TP[2],TP[2],TP[2],TP[2],TP[2]+2],'purple')
 plt.plot([TP[0],P1[0],P2[0]],[TP[1],P1[1],P2[1]],[TP[2],P1[2],P2[2]],'green')
 plt.plot([P1[0],10*N[0]/mg(N)+P1[0]],[P1[1],10*N[1]/mg(N)+P1[1]],[P1[2],10*N[2]/mg(N)+P1[2]],'blue')
-plt.plot([P2[0],P3[0],O[0]],[P2[1],P3[1],O[1]],[P2[2],P3[2],O[2]],'orange')
-plt.plot([O[0],0],[O[1],0],[O[2],0],'pink')
-plt.plot([TP[0], TRV[0]+TP[0]],[TP[1], TRV[1]+TP[1]],[TP[2], TRV[2]+TP[2]],'blue')
-ax.plot3D([0,-70,-70,-70,70,70],[0,0,-70,70,70,70],[0,0,0,0,0,140],'gray')
+plt.plot([P2[0],P3[0],P4[0]],[P2[1],P3[1],P4[1]],[P2[2],P3[2],P4[2]],'orange')
+plt.plot([P4[0],0],[P4[1],0],[P4[2],0],'pink')
+plt.plot([P2[0],P1[0]],[P2[1],P1[1]],[P2[2],P2[2]+(P1[0]-P2[0])*(P2[2]-P3[2])/(P2[0]-P3[0])],'red')
+ax.plot3D([0,-a,-a,-a,a,a],[0,0,-a,a,a,a],[0,0,0,0,0,2*a],'gray')
 plt.draw()
 plt.show()
